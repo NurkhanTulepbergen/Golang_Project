44d4ac11678a3fa55fc086bdb4f474e7c8bff0f5
Ayazhan Taimas <a_taimas@kbtu.kz>
2024-02-24 16:36:40 +0600

    my change

---
+++ pkg/migration/create_tables.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS user
+(
+    id          bigserial PRIMARY KEY,
+    created_at  timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at  timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    name       text                        NOT NULL,
+    password text                        NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products
+(
+    id              bigserial PRIMARY KEY,
+    created_at      timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at      timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    title           text                        NOT NULL,
+    description     text,
+    price int
+);
+create table if not exists category
+(
+    id              bigserial PRIMARY KEY,
+    created_at      timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at      timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    title           text                        NOT NULL,
+    description     text
+);
+CREATE TABLE IF NOT EXISTS category_and_products
+(
+    "id"         bigserial PRIMARY KEY,
+    "created_at" timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    "updated_at" timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    "category" bigserial,
+    "products"       bigserial,
+    FOREIGN KEY (category)
+        REFERENCES products(id),
+    FOREIGN KEY (products)
+        REFERENCES category(id)
+);
---
+++ pkg/migration/drop_tables.sql
@@ -0,0 +1,4 @@
+drop table if exists users;
+drop table if exists products;
+drop table if exists catalog;
+drop table if exists catalog_and_products;
