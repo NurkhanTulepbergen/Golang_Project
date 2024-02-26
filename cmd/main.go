0589fc784df854a6345298dce0a28fedb86ee8c7
Zhasmin2004 <zh_suleimenova@kbtu.kz>
2024-02-26 11:44:42 +0600

    zhas

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,40 @@
 package cmd
+
+import (
+	"fmt"
+)
+
+type Product struct {
+	ID   int
+	Name string
+}
+
+type Shop struct {
+	ID   int
+	Name string
+}
+
+type Models struct {
+	Product Product
+	Shop    Shop
+}
+
+func NewModels() Models {
+	return Models{
+		Product: Product{
+			ID:   1,
+			Name: "Product 1",
+		},
+		Shop: Shop{
+			ID:   1,
+			Name: "Shop 1",
+		},
+	}
+}
+
+func main() {
+	models := NewModels()
+
+	fmt.Println("Product:", models.Product)
+	fmt.Println("Shop:", models.Shop)
+}
