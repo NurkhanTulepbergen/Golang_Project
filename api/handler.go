e02df7513c6ad4e1239236163595bf27f6cc88fe
AzamatTaubayev <a_taubayev@kbtu.kz>
2024-02-26 12:26:53 +0600

    Azamat Erbolatuly

--- api/handler.go
+++ api/handler.go
@@ -1,1 +1,31 @@
 package api
+
+import (
+	"Golang_Project/pkg/model"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Catalog []model.Product `json:"catalog"`
+}
+
+func StartServer() {
+	router := mux.NewRouter()
+	log.Println("creating routes")
+	router.HandleFunc("/health-check", HealthCheck).Methods("GET")
+	router.HandleFunc("/catalog", Catalog).Methods("GET")
+	http.Handle("/", router)
+	http.ListenAndServe(":2003", router)
+}
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	log.Println("welcome")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello there")
+}
+func Catalog(w http.ResponseWriter, r *http.Request) {
+	log.Println("books")
+	fmt.Fprintf(w, "I love J.S")
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,40 +1,39 @@
-package cmd
+package main
 
 import (
-	"fmt"
+	"Golang_Project/api"
+	"log"
 )
 
-type Product struct {
-	ID   int
-	Name string
-}
-
-type Shop struct {
-	ID   int
-	Name string
-}
-
-type Models struct {
-	Product Product
-	Shop    Shop
-}
-
-func NewModels() Models {
-	return Models{
-		Product: Product{
-			ID:   1,
-			Name: "Product 1",
-		},
-		Shop: Shop{
-			ID:   1,
-			Name: "Shop 1",
-		},
-	}
-}
+//type Product struct {
+//	ID   int
+//	Name string
+//}
+//
+//type Shop struct {
+//	ID   int
+//	Name string
+//}
+//
+//type Models struct {
+//	Product Product
+//	Shop    Shop
+//}
+//
+//func NewModels() Models {
+//	return Models{
+//		Product: Product{
+//			ID:   1,
+//			Name: "Product 1",
+//		},
+//		Shop: Shop{
+//			ID:   1,
+//			Name: "Shop 1",
+//		},
+//	}
+//}
 
 func main() {
-	models := NewModels()
-
-	fmt.Println("Product:", models.Product)
-	fmt.Println("Shop:", models.Shop)
+	log.Println("kickstart my heart")
+	api.StartServer()
 }
